27c6e91f707e4a1b6db7d0083997ec508206ec6f
Rakibei <rakibei@outlook.dk>
2025-09-29 10:32:17 +0200

    Spring Boot Setup

---
+++ src/main/java/semester3/project/RedirectController.java
@@ -0,0 +1,14 @@
+package semester3.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RedirectController {
+
+    @GetMapping("/")
+    public String redirectToNew() {
+        // Redirects to /new
+        return "redirect:/hello";
+    }
+}
