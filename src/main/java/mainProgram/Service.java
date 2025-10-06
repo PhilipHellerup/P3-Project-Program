3e8ea8b2e7302bbe22e83d33db19d01f7ade4334
Daniel Sloth <131558433+dani156f@users.noreply.github.com>
2025-10-06 11:07:42 +0200

    Service.java

--- src/main/java/mainProgram/Service.java
+++ src/main/java/mainProgram/Service.java
@@ -1,5 +1,31 @@
 package mainProgram;
 
+
+// Service class
 public class Service {
+    // Attributes
+    private String name;
     private double price;
+    private int duration;
+
+    // Constructor
+    public Service(String name, double price, int serviceDuration){
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters
+    public String getServiceName(){
+        return name;
+    }
+
+    public double getServicePrice(){
+        return price;
+    }
+
+    public int getServiceDuration(){
+        return duration;
+    }
+
+    // Setters
 }
