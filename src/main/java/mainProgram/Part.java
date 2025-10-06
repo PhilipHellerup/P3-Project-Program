c45f2c330168a2ad55e16a3bee2859aae5e5e1d5
ViktorParkhoi <vparkh24@student.aau.dk>
2025-10-06 10:57:18 +0200

    Added Part and Product classes

---
+++ src/main/java/mainProgram/Part.java
@@ -0,0 +1,30 @@
+package mainProgram;
+
+public class Part {
+    private String name;
+    private String type;
+
+    // Constructor
+    public Part(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+    
+    public String getType() {
+        return type;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
