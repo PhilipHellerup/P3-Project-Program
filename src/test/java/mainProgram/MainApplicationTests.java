6dc7087052f672e3258183f68850dc303bf3b0b5
Anders Mathias Larsen <151789447+Suppedini@users.noreply.github.com>
2025-11-10 11:03:33 +0100

    et par tests lavet
    2 ud af 5 tests lavet, "kan laves mange flere ofc" de 5 er tænkt som simple og nødvendige tests

--- src/test/java/mainProgram/MainApplicationTests.java
+++ src/test/java/mainProgram/MainApplicationTests.java
@@ -1,14 +1,30 @@
 package mainProgram;
 
+import mainProgram.repository.JobRepository;
+import mainProgram.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import mainProgram.table.Product;
+import org.springframework.context.ApplicationContext;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
 class MainApplicationTests {
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+        assertNotNull(applicationContext);
+    }
+
+    @Test
+    void testRepositoryBeansAreLoaded() {
+        assertNotNull(applicationContext.getBean(JobRepository.class));
+        assertNotNull(applicationContext.getBean(ProductRepository.class));
+    }
 }
+
+
