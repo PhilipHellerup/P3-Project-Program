36f8863310ca2aa6c8ea16d8941b6085dd2317f8
Anders Mathias Larsen <151789447+Suppedini@users.noreply.github.com>
2025-11-10 10:13:31 +0100

    Update MainApplicationTests.java

--- src/test/java/mainProgram/MainApplicationTests.java
+++ src/test/java/mainProgram/MainApplicationTests.java
@@ -2,6 +2,9 @@ package mainProgram;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import mainProgram.table.Product;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 class MainApplicationTests {
