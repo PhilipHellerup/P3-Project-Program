64e556f9bfa3841469cf4a769437cac5e6da8320
PhilipHellerupAAU <pjorge24@student.aau.dk>
2025-12-01 11:35:46 +0100

    Finished Moving Business Logic From Controller Layer to Service Layer

--- src/main/java/mainProgram/services/BaseSearchService.java
+++ src/main/java/mainProgram/services/BaseSearchService.java
@@ -1,9 +1,11 @@
-package mainProgram.services;
+package mainProgram.services; // Project Organization
 
+/* --- Imports */
 import java.util.List;
 
-///  Each service where we want to be able to search implements this base searchService.
-
+/* --- BaseSearchService Interface --- */
+// Each service where we want to be able to search, implements this base searchService
+// (Contract = Must Implement `search` Method).
 public interface BaseSearchService<T> {
     List<T> search(String keyword);
 }
