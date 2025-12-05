dc06ed2cfb03d245085a50cc252d9cf066a6c528
JacobCLarsen <jcla24@student.aau.dk>
2025-12-05 10:02:56 +0100

    Update getRepairDetailsTest.java

--- src/test/java/mainProgram/intergrationTest/getRepairDetailsTest.java
+++ src/test/java/mainProgram/intergrationTest/getRepairDetailsTest.java
@@ -1,27 +1,47 @@
 package mainProgram.intergrationTest;
 
+import mainProgram.repository.JobPartRepository;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class getRepairDetailsTest {
     @Autowired
     MockMvc mockMvc;
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private Integer jobId;
+
     @Test
     void jobTitleAppearsInRenderedHtml() throws Exception {
-        MvcResult result = mockMvc.perform(get("/jobliste/20"))
+        int id = createTestRepairGetId();
+        MvcResult result = mockMvc.perform(get("/jobliste/" + id))
                 .andExpect(status().isOk())
                 .andReturn();
 
@@ -31,12 +51,13 @@ public class getRepairDetailsTest {
         // Check the element where the title is rendered
         String title = doc.select("#job-title").text();
 
-        assertEquals("bike1test", title);
+        assertEquals("Test repair 1", title);
     }
 
     @Test
     void jobCustomerNameAppearsInRenderedHtml() throws Exception {
-        MvcResult result = mockMvc.perform(get("/jobliste/20"))
+        int id = createTestRepairGetId();
+        MvcResult result = mockMvc.perform(get("/jobliste/" + id))
                 .andExpect(status().isOk())
                 .andReturn();
 
@@ -46,12 +67,13 @@ public class getRepairDetailsTest {
         // Check the element where the customer name is rendered
         String name = doc.select("#job-customer-name").text();
 
-        assertEquals("test3", name);
+        assertEquals("John Doe", name);
     }
 
     @Test
     void jobCustomerPhoneAppearsInRenderedHtml() throws Exception {
-        MvcResult result = mockMvc.perform(get("/jobliste/20"))
+        int id = createTestRepairGetId();
+        MvcResult result = mockMvc.perform(get("/jobliste/" + id))
                 .andExpect(status().isOk())
                 .andReturn();
 
@@ -61,6 +83,34 @@ public class getRepairDetailsTest {
         // Check the element where the phone number is rendered
         String phone = doc.select("#job-customer-phone").text();
 
-        assertEquals("44444444", phone);
+        assertEquals("12345678", phone);
+    }
+
+    public int createTestRepairGetId(){
+            // Create a repair
+            Map<String, Object> repair = new HashMap<>();
+            repair.put("title", "Test repair 1");
+            repair.put("customer_name", "John Doe");
+            repair.put("customer_phone", "12345678");
+            repair.put("job_description", "Broken chain");
+            repair.put("work_time_minutes", 30);
+            repair.put("price_per_min", 5.0);
+            repair.put("duration", 60);
+            repair.put("date", "2025-10-28T13:45:00");
+            repair.put("status", Map.of("id", 1));
+
+            ResponseEntity<Map> repairResp =
+                    restTemplate.postForEntity(
+                            "http://localhost:" + port + "/api/jobs/create",
+                            repair,
+                            Map.class
+                    );
+
+            assertEquals(HttpStatus.OK, repairResp.getStatusCode());
+            jobId = (Integer) repairResp.getBody().get("id");
+            assertNotNull(jobId);
+
+            return jobId;
     }
 }
+
